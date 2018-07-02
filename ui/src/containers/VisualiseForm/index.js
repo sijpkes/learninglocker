f06880688f4c36bf14f096e379ead6df9a2917f7
Chris Bishop <chrisjbishop155@gmail.com>
2018-07-02 10:04:39 +0100

    Start of visualisations.

--- ui/src/containers/VisualiseForm/index.js
+++ ui/src/containers/VisualiseForm/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import StatementsForm from './StatementsForm/StatementsForm';
+import Visualisation from '@learninglocker/visualisations/dist/components/Visualisation';
+import { updateModel } from 'ui/redux/modules/models';
 
-const VisualiseForm = ({ model, exportVisualisation }) =>
+export const VisualiseFormOld = ({ model, exportVisualisation }) =>
   <StatementsForm model={model} exportVisualisation={exportVisualisation} />;
+
+const VisualiseForm = ({ model }) => {
+  console.log('001');
+  // return <div>001</div>;
+  const actions = {
+    updateModel
+  };
+
+  return <Visualisation model={model} actions={actions} />;
+};
 
 export default VisualiseForm;
